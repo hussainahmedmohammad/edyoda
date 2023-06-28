c25efeef61ea982b0dfda16b56f95530532996a9
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-06-28 22:50:58 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,16 @@
+# Write a Python program that accepts a word from the user and reverse it.
+
+
+# Input : Edyoda
+# output: adoydE
+
+# a = "string"
+# print (a[2:5:1])
+
+name = input()
+rev = name[::-1]
+print(rev)
+
+
+
+
