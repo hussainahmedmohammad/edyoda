d932fd1624d85d261ac94344c89af3cf426f9761
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-06-29 17:17:51 +0530

    Add files via upload

---
+++ counteven_odd.py
@@ -0,0 +1,18 @@
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+
+# Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+# Expected Output :
+# Number of even numbers : 4
+# Number of odd numbers : 5
+
+
+even = 0
+odd = 0
+num = [1,2,3,4,5,6,7,8,9]
+for i in num:
+    if i % 2 == 0:
+        even = even + 1
+    else:
+        odd = odd + 1
+print("Number of even numbers :", even)
+print("Number of even numbers :", odd)
