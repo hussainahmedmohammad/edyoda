ac144e7a7e357f616bf64dc4fd554cf0dd9823c0
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-06-29 17:32:12 +0530

    Add files via upload

--- counteven_odd.py
+++ counteven_odd.py
@@ -5,14 +5,43 @@
 # Number of even numbers : 4
 # Number of odd numbers : 5
 
+#with list
 
-even = 0
-odd = 0
-num = [1,2,3,4,5,6,7,8,9]
-for i in num:
-    if i % 2 == 0:
-        even = even + 1
-    else:
-        odd = odd + 1
-print("Number of even numbers :", even)
-print("Number of even numbers :", odd)
+# even = 0
+# odd = 0
+# num = [1,2,3,4,5,6,7,8,9]
+# for i in num:
+#     if i % 2 == 0:
+#         even = even + 1
+#     else:
+#         odd = odd + 1
+# print("Number of even numbers :", even)
+# print("Number of even numbers :", odd)
+
+
+# with tuple
+
+# even = 0
+# odd = 0
+# num = (1,2,3,4,5,6,7,8,9)
+# for i in num:
+#     if i % 2 == 0:
+#         even = even + 1
+#     else:
+#         odd = odd + 1
+# print("Number of even numbers :", even)
+# print("Number of even numbers :", odd)
+
+
+# with for loop
+
+# even = 0
+# odd = 0
+# for i in range(1,10):
+#     if i % 2 == 0:
+#         even += 1
+#     else:
+#         odd +=1
+# print("Number of even numbers :", even)
+# print("Number of even numbers :", odd)
+
