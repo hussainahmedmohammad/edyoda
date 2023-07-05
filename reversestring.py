b637eb502f58afa49f2ce0004f84ce1b3e1a12aa
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-07-05 23:37:04 +0530

    reversestring.py

--- reversestring.py
+++ reversestring.py
@@ -7,9 +7,27 @@
 # a = "string"
 # print (a[2:5:1])
 
-name = input()
-rev = name[::-1]
-print(rev)
+# name = input()
+# rev = name[::-1]
+# print(rev)
+
+# while loop
+# name = input("entre a word : ")
+# reverse = ''
+# index = len(name)-1 
+# while index >= 0:
+#     reverse = reverse + name[index]
+#     index = index - 1
+# print(reverse)
+
+
+#for loop
+# name = input("entre a word : ")
+# reverse = ''
+# pro = name[::-1]
+# for i in pro:
+#     print(i,end="")
+
 
 
 
