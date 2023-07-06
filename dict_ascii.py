b57071051080c6bc7d215d257c2b8d6db8447392
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-07-06 16:29:56 +0530

    Add files via upload

---
+++ dict_ascii.py
@@ -0,0 +1,40 @@
+# assignment - 2
+
+# write a python program to create a dictnory whose keys 
+# should be alpabets and values should be corresponding ascii values of it
+
+# objectives : 
+# ---> must not entred key value pair manually
+# ---> should print the excute value 
+
+# expected output: 
+#     {'a': 97,'b': 98, ......}
+
+# print(chr(97)) -->a
+# print(ord('a')) --> 97
+
+
+# for loop
+
+# dict = {}
+# for i in range (97,123):
+#     char = chr(i)
+#     dict[char] = i
+    
+# print(dict)
+
+
+# while_loop
+
+# dict = {}
+# ascii = 97
+# while ascii < 123:
+#     char = chr(ascii)
+#     dict[char] = ascii
+#     ascii += 1
+# print(dict)
+    
+
+
+ 
+        
