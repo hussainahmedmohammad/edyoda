34cf0e7db425b729ec5fb375307b81f1182db89b
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-07-11 16:13:25 +0530

    Update count_upper&lower_char.py

--- assignments/count_upper&lower_char.py
+++ assignments/count_upper&lower_char.py
@@ -6,22 +6,13 @@
 # No. of Upper case characters : 3
 # No. of Lower case Characters : 12
 
+#objectives:
+# 1> code must be implemented by using function
+# 2> fun must be accept the string
+# 3> count of upper case must be printed
+# 4> count of lower case must be printed
 
-#without function
-# string = input("entre a string : ")
-# uc = 0
-# lc = 0
-# total = 0
-# for i in string:
-#     if i.isupper() == True:
-#         uc += 1
-#     elif i.islower() == True:
-#         lc += 1   
-# print ("No. of Upper case characters :",uc) #3
-# print ("No. of Lower case Characters :",lc) #12
-
-
-# with function:
+# with function method 1:
 
 # def count(string):
 #     uc = 0
@@ -31,8 +22,43 @@
 #             uc += 1
 #         elif i.islower() == True:
 #             lc += 1
-#     return (uc,lc)
+#     print ("No. of Upper case characters :",uc ) 
+#     print ("No. of Lower case Characters :",lc) 
+#     return
+# string = input("entre a string : ")
+# count(string)
 
-# string = 'The quick Brow Fox'#input("entre a string : ")
-# print("No. of Upper case characters :",count(string))
-# print("No. of Lower case Characters :",count(string))
+
+
+#with function method-2
+# string = input("entre a string : ")#'The quick Brow Fox'
+# def words():
+#      return string
+# def count(string):
+#     uc = 0
+#     lc = 0
+#     for i in words():
+#         if i.isupper() == True:
+#             uc += 1
+#         elif i.islower() == True:
+#             lc += 1
+#     print ("No. of Upper case characters :",uc) #3
+#     print ("No. of Lower case Characters :",lc)
+#     return
+# count(words())
+    
+
+#without function
+
+# string = input("entre a string : ")
+# uc = 0
+# lc = 0
+# total = 0
+# for i in string:
+#     if i.isupper() == True:
+#         uc += 1
+#     elif i.islower() == True:
+#         lc += 1   
+# print ("No. of Upper case characters :",uc) 
+# print ("No. of Lower case Characters :",lc) 
+
