904d4c9857cb97fd3bab80c53fa28eac85728c85
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-08-06 00:34:26 +0530

    Update assignment_json_part_2.py

--- assignment_json_folder/assignment_json_part_2.py
+++ assignment_json_folder/assignment_json_part_2.py
@@ -1,9 +1,6 @@
-# 1. Create a JSON file (employee.json) containing employee 
-# information of minimum 5 employees. Each employee 
-# information consists of Name, DOB, Height, City, State.
-# Write a python program that reads this information from 
-# the JSON file and saves the information into a list of 
-# objects of Employee class. Finally print the list of the 
+# 1. Create a JSON file (employee.json) containing employee information of minimum 5 employees. Each employee 
+# information consists of Name, DOB, Height, City, State. Write a python program that reads this information from 
+# the JSON file and saves the information into a list of  objects of Employee class. Finally print the list of the 
 # Employee objects.
 
 # created employee.json file with assignment_json_1.py 
