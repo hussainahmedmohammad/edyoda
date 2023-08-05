353492c8163546073a70dcf20eceee7bc987bfb1
Hussain Ahmed <127744585+hussainahmedmohammad@users.noreply.github.com>
2023-08-06 00:31:01 +0530

    Update assignment_dog.py

--- assignment_json_folder/assignment_dog.py
+++ assignment_json_folder/assignment_dog.py
@@ -1,15 +1,11 @@
-# 1. Create a class named ‘Dog’. It should have a constructor
-# which accepts its name, age and coat color. You must perform
+# 1. Create a class named ‘Dog’. It should have a constructor which accepts its name, age and coat color. You must perform
 # the following operations:
 
-#  a. It should have a function ‘description()’ which prints
-# the name and age of the dog.
+#  a. It should have a function ‘description()’ which prints the name and age of the dog.
  
-#  b. It should have a function ‘get_info()’ which prints 
-# the coat color of the dog.
+#  b. It should have a function ‘get_info()’ which prints the coat color of the dog.
  
-#  c. Create child classes ‘JackRussellTerrier’ and ‘Bulldog’ 
-#  which is inherited from the class ‘Dog’. 
+#  c. Create child classes ‘JackRussellTerrier’ and ‘Bulldog’  which is inherited from the class ‘Dog’. 
 #  It should have at least two methods of its own.
  
 #  d. Create objects and implement the above functionalities.
